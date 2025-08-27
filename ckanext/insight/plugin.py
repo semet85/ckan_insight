ea029e4d2076b7a8e9c771a7e5be18f95bdaac7a
Hanny <semethanny@gmail.com>
2025-08-27 20:44:07 +0800

    fsdf

--- ckanext/insight/plugin.py
+++ ckanext/insight/plugin.py
@@ -12,17 +12,15 @@ from .lib.insight_groups import (
 
 log = logging.getLogger(__name__)
 
-
 def _get_insight_tag_from_config():
     return toolkit.config.get('ckanet.insight_tag', 'insight')
-
 
 class InsightPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
     plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IClick)
-    plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.ITemplateHelpers)   # <-- penting untuk helpers
 
     # IConfigurer
     def update_config(self, config_):
@@ -55,29 +53,27 @@ class InsightPlugin(plugins.SingletonPlugin):
             pass
 
         @insight.command('sync-groups')
-        @click.option(
-            '--tag', 'insight_tag', default=None,
-            help='Marker tag for insight datasets (default: "insight")'
-        )
+        @click.option('--tag', 'insight_tag', default=None,
+                      help='Marker tag for insight datasets (default: "insight")')
         def sync_groups_cmd(insight_tag):
             if not insight_tag:
                 insight_tag = _get_insight_tag_from_config()
-            click.echo('Rebuilding insight groups with marker tag "%s"...' % insight_tag)
+            click.echo(f'Rebuilding insight groups with marker tag "{insight_tag}"...')
             stats = rebuild_all_insight_groups(insight_tag)
-            click.echo(
-                "Done: created_groups=%s, updated_links=%s, removed_links=%s" %
-                (stats['created_groups'], stats['updated_links'], stats['removed_links'])
-            )
+            click.echo(f"Done: created_groups={stats['created_groups']}, "
+                       f"updated_links={stats['updated_links']}, removed_links={stats['removed_links']}")
 
-        return [insight]  # penting: return list of click groups
+        return [insight]
 
     # ITemplateHelpers
     def get_helpers(self):
-        # pastikan helpers.py punya dua fungsi ini
-        from .helpers import ckanet_insight_list, ckanet_count_groups_startswith, 
-        ckanet_org_counts,
+        from .helpers import (
+            ckanet_insight_list,
+            ckanet_count_groups_startswith,
+            ckanet_org_counts,  # kalau kamu sudah tambahkan helper ini
+        )
         return {
-            'ckanet_insight_list': ckanet_insight_list,
-            'ckanet_count_groups_startswith': ckanet_count_groups_startswith,
+            "ckanet_insight_list": ckanet_insight_list,
+            "ckanet_count_groups_startswith": ckanet_count_groups_startswith,
             "ckanet_org_counts": ckanet_org_counts,
         }
